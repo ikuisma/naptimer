454386f5e7d027c7e52429a5d3d6dd264fddc086
ikuisma <ilkka.a.kuisma@gmail.com>
2019-04-13 18:22:19 +0300

    Quick and dirty naptimer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    marginTop: '1rem',
+    maxWidth: '600px',
+    margin: 'auto'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '100%'
+  },
+});
+
+const mins = str => {
+  const [shour, smin] = str.split(':')
+  return Number(shour) * 60 + Number(smin)
+}
 
 class App extends Component {
+  state={
+    start: '08:30',
+    end: '09:30'
+  }
+  getDifference = () => {
+    const { start, end } = this.state
+    const minutes = mins(end) - mins(start)
+    return minutes
+  }
+  updateTime = ({ target }) => this.setState({ [target.id]: target.value })
+
   render() {
+    const { classes } = this.props
+    const { start, end } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.container}>
+          <Typography variant="h4" gutterBottom>
+            Bubz Naptimer
+          </Typography>
+          <TextField
+            id="start"
+            label="Started"
+            type="time"
+            value={start}
+            className={classes.textField}
+            onChange={this.updateTime}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              step: 300, // 5 min
+            }}
+          />
+          <TextField
+            id="end"
+            label="Ended"
+            type="time"
+            value={end}
+            className={classes.textField}
+            onChange={this.updateTime}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              step: 300, // 5 min
+            }}
+          />
+          <p>{`Bubz napped for ${this.getDifference()} minutez!`}</p>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
